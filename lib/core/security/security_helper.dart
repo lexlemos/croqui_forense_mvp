205e82a3d65ee0545f999915ad96baaa0fd6ff36
lexlemos <allexlemos12@gmail.com>
2025-12-16 17:45:21 -0300

    refact: troca do codigo de criptografia manual pelo uso da lib pointycastle

--- lib/core/security/security_helper.dart
+++ lib/core/security/security_helper.dart
@@ -1,12 +1,16 @@
 import 'dart:convert';
 import 'dart:math';
 import 'dart:typed_data';
-import 'package:crypto/crypto.dart';
+
+import 'package:pointycastle/api.dart';
+import 'package:pointycastle/digests/sha256.dart';
+import 'package:pointycastle/key_derivators/pbkdf2.dart';
+import 'package:pointycastle/macs/hmac.dart';
+import 'package:pointycastle/key_derivators/api.dart';
 
 class SecurityHelper {
-
-  static const int _iterations = 10000; 
-  static const int _keyLength = 32;
+  static const int _iterations = 10000;
+  static const int _keyLength = 32; 
   static const int _saltLength = 16;
 
   static String generateSalt() {
@@ -20,60 +24,28 @@ class SecurityHelper {
 
   static String hashPin(String pin, String salt) {
     final saltBytes = base64Decode(salt);
-    final pinBytes = utf8.encode(pin);
+    final pinBytes = utf8.encode(pin); 
 
-    final derivedKey = _pbkdf2(pinBytes, saltBytes, _iterations, _keyLength);
-    
+    final derivator = PBKDF2KeyDerivator(HMac(SHA256Digest(), 64));
+
+    final params = Pbkdf2Parameters(saltBytes, _iterations, _keyLength);
+    derivator.init(params);
+    final derivedKey = derivator.process(Uint8List.fromList(pinBytes));
+
     return base64Encode(derivedKey);
   }
 
   static bool verifyPin(String inputPin, String storedHash, String storedSalt) {
     try {
       final newHash = hashPin(inputPin, storedSalt);
+      
       return _constantTimeCompare(newHash, storedHash);
     } catch (e) {
       return false;
     }
   }
 
-  static Uint8List _pbkdf2(List<int> password, List<int> salt, int iterations, int keyLength) {
-    final hmac = Hmac(sha256, password);
-    final derivedKey = Uint8List(keyLength);
-    final blockLength = hmac.convert([]).bytes.length;
-    final totalBlocks = (keyLength / blockLength).ceil();
-
-    int offset = 0;
-    for (int block = 1; block <= totalBlocks; block++) {
-      var u = hmac.convert([...salt, ..._intTo4Bytes(block)]).bytes;
-      var t = Uint8List.fromList(u);
-
-      for (int i = 1; i < iterations; i++) {
-        u = hmac.convert(u).bytes;
-        for (int j = 0; j < u.length; j++) {
-          t[j] ^= u[j];
-        }
-      }
-
-      final copyLen = min(blockLength, keyLength - offset);
-      for (int i = 0; i < copyLen; i++) {
-        derivedKey[offset + i] = t[i];
-      }
-      offset += copyLen;
-    }
-
-    return derivedKey;
-  }
-
-  static List<int> _intTo4Bytes(int value) {
-    return [
-      (value >> 24) & 0xff,
-      (value >> 16) & 0xff,
-      (value >> 8) & 0xff,
-      value & 0xff,
-    ];
-  }
-
- static bool _constantTimeCompare(String a, String b) {
+  static bool _constantTimeCompare(String a, String b) {
     final aBytes = utf8.encode(a);
     final bBytes = utf8.encode(b);
 
